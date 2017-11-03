084f99e200dbd81dc6fc945e3b6229c72b67b792
sbouii <mariem.sbouiii40@gmail.com>
2017-11-03 12:32:43 +0100

    Update start.sh

--- git_server/start.sh
+++ git_server/start.sh
@@ -1,6 +1,7 @@
 # Fix rights for files
 chmod 755 /home/git/.ssh
 chmod 644 /home/git/.ssh/authorized_keys
+#change the /home/git directory owner
 chown -R git:git /home/git
 # Launching open-ssh server
 /usr/sbin/sshd -D
