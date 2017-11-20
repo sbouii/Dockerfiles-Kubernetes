cd98246a231006bdb3625731433d76162c640a48
sbouii <mariemsbouiii40@gmail.com>
2017-11-20 11:18:52 +0100

    config haproxy

--- git_server/start.sh
+++ git_server/start.sh
@@ -1,7 +1,14 @@
 # Fix rights for files
-chmod 755 /home/git/.ssh
-chmod 644 /home/git/.ssh/authorized_keys
+chmod 700 /home/git/.ssh
+chmod 600 /home/git/.ssh/authorized_keys
 #change the /home/git directory owner
 chown -R git:git /home/git
-# Launching open-ssh server
+#create the host key if it's not already created
+HOSTKEY=/etc/ssh/ssh_host_ed25519_key
+if [ ! -f "${HOSTKEY}" ]; then
+  ssh-keygen -A
+fi
+cat /etc/ssh/ssh_host_ed25519_key > /home/git/.ssh/authorized_keys
+# Launching sshd 
+# -D flag avoids executing sshd as a daemon in order to avoid accepting connections in background
 /usr/sbin/sshd -D
